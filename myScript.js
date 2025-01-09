052274117e86792445ad1e5ffd6add8fb6b6bbae
GergesTammo2001 <gorgos2002@icloud.com>
2025-01-09 12:53:27 +0100

    første nettsiden min

---
+++ myScript.js
@@ -0,0 +1,13 @@
+function mobilMenu(){
+
+			var x = document.getElementById("ham");
+
+			if(x.style.display === "block"){
+				x.style.display = "none";
+			}
+			
+			else {
+				x.style.display= "block";
+			}
+
+}
